ba5de89f4a42d14032372e892f825903349a18bc
Evan <abracadabraunnoire@gmail.com>
2025-02-17 10:49:26 +0100

    update initialisation du site via

---
+++ sript.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-incon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+}
